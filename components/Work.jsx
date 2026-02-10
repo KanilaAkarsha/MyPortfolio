62d2fc336cc34050c0ee366c28ce93f2b76be873
Kanila Akarsha <156494340+KanilaAkarsha@users.noreply.github.com>
2026-02-10 20:53:28 +0530

    up

--- components/Work.jsx
+++ components/Work.jsx
@@ -44,14 +44,15 @@ export default function Work() {
             style={{ backgroundImage: `url(${work.icon})` }}>
             <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
               <div>
-                <a href={work.link} target="_blank" rel="noopener noreferrer">
-                  <h2 className="font-semibold">{work.name}</h2>
-                </a>
+                <h2 className="font-semibold">{work.name}</h2>
+
                 <p className="text-sm text-gray-700">{work.description}</p>
               </div>
-              <div className="border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-300 transition">
-                <img src="/assets/send-icon.png" alt="" className="w-5" />
-              </div>
+              <a href={work.link} target="_blank" rel="noopener noreferrer">
+                <div className="border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-300 transition">
+                  <img src="/assets/send-icon.png" alt="" className="w-5" />
+                </div>
+              </a>
             </div>
           </div>
         ))}
